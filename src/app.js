e601b1d1a5c56471e6745eafe1e9fbd97e17bd97
Yash Wakde <yashwakde32@gmail.com>
2025-09-25 23:05:54 +0530

    update the app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,21 @@ import promptroute from "./routes/prompt.route.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://promptfrontend-two.vercel.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps, curl)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 
